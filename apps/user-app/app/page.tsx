f8050f41782dee416621e2233c052c23f030e061
Faizan <faizansohail077@gmail.com>
2024-03-24 16:14:40 +0500

    initialized tailwind css

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@repo/ui/Input";
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl text-red-600 ">
+        Initialized tailwind css
+      </h1>
+        <Input />
+    </div>
+  );
+}
